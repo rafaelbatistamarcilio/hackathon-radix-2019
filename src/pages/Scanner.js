d4c0d5b5501eab5e8d854649e62188a419d45507
rafael marcilio <rafaelbatistamarcilio@gmail.com>
2019-07-06 23:40:57 -0300

    correção na extração do codigo do qrcode

--- src/pages/Scanner.js
+++ src/pages/Scanner.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import QrReader from 'react-qr-reader';
 
-
-
+const APP_QRCODE = 'https://hackthon-radix-cpfl.herokuapp.com/chat?id=';
 
 export default props => {
 
     const handleScan = data => {
         if (data) {
-            props.history.push('chat?uid='+data);
+            props.history.push('chat?id='+ data.replace(APP_QRCODE,''));
         }
     }
     const handleError = err => {
